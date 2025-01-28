54eecb09ee11e8f143f1acb24d78d127fc2497a7
Mamadsalo <mamadsaloiboqi@gmail.com>
2025-01-28 20:05:38 +0500

    First commit

---
+++ js_step_1/js/script.js
@@ -0,0 +1,16 @@
+// console.log(1);
+// alert("Hello wordld");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Are you 18?");
+// console.log(typeof answer);
+
+// const answers = [];
+
+// answers[0] = prompt("What is your name?", "");
+// answers[1] = prompt("What is your name?", "");
+// answers[2] = prompt("How old are you?", "");
+
+// document.write(typeof answers);
